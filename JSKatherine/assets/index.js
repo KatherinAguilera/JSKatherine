d8bdd5743be5b9d8aef6ff92b258b41f09c022ae
Katherine Aguilera <katherinam35@gmail.com>
2019-08-20 17:11:40 -0500

    Plugin

--- JSKatherine/assets/index.js
+++ JSKatherine/assets/index.js
@@ -1,25 +1,17 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
-function MediaPlayer(config) {
-  this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function() {
-  this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
-  this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function() {
-  if (this.media.paused) {
-    this.play();
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
   } else {
-    this.pause();
+    player.mute();
   }
 };
