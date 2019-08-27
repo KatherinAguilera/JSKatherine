0663b5aeb269da8d5c98b52099e379c2ce13847c
Katherine Aguilera <katherinam35@gmail.com>
2019-08-27 12:30:16 -0500

    serviworker

--- JSKatherine/assets/index.js
+++ JSKatherine/assets/index.js
@@ -20,3 +20,8 @@ muteButton.onclick = () => {
     player.mute();
   }
 };
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
